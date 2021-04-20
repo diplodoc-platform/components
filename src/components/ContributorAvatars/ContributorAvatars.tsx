60213052d607f6a9f04018a722eb22a90ab7e215
Natalia Novikova <novikovans94@gmail.com>
2021-04-20 14:49:02 +0300

    Author -> contributors

--- src/components/ContributorAvatars/ContributorAvatars.tsx
+++ src/components/ContributorAvatars/ContributorAvatars.tsx
@@ -9,8 +9,8 @@ import './ContributorAvatars.scss';
 
 const b = block('contributor-icons');
 
-const MAX_DISPLAYED_AUTHORS = 3;
-const LOWER_BOUND_MORE_AUTHORS = 10;
+const MAX_DISPLAYED_CONTRIBUTORS = 3;
+const LOWER_BOUND_MORE_CONTRIBUTORS = 10;
 
 export interface ContributorAvatarsProps {
     contributors: Contributor[];
@@ -32,18 +32,18 @@ const ContributorAvatars: React.FC<ContributorAvatarsProps> = (props) => {
     );
 };
 
-function getAvatars(authors: Contributor[], isAuthor = false, vcsType?: Vcs): ReactElement {
-    if (authors.length === 1) {
-        return getOneAvatar(authors[0], isAuthor, vcsType);
+function getAvatars(contributors: Contributor[], isAuthor = false, vcsType?: Vcs): ReactElement {
+    if (contributors.length === 1) {
+        return getOneAvatar(contributors[0], isAuthor, vcsType);
     }
 
-    const displayedAuthors = [...authors];
-    const hiddenAuthors = displayedAuthors.splice(MAX_DISPLAYED_AUTHORS);
+    const displayedContributors = [...contributors];
+    const hiddenContributors = displayedContributors.splice(MAX_DISPLAYED_CONTRIBUTORS);
 
-    const displayedAvatars = displayedAuthors.map((author: Contributor) => {
-        return getAvatar(author, vcsType);
+    const displayedAvatars = displayedContributors.map((contributor: Contributor) => {
+        return getAvatar(contributor, vcsType);
     });
-    const hiddenAvatars = getHiddenAvatars(hiddenAuthors);
+    const hiddenAvatars = getHiddenAvatars(hiddenContributors);
 
     return (
         <Fragment>
@@ -53,55 +53,55 @@ function getAvatars(authors: Contributor[], isAuthor = false, vcsType?: Vcs): Re
     );
 }
 
-function getOneAvatar(author: Contributor, isAuthor: boolean, vcsType?: Vcs): ReactElement {
-    const userName = isAuthor
-        ? author.name
-        : getShortUserName(author.name);
+function getOneAvatar(contributor: Contributor, isAuthor: boolean, vcsType?: Vcs): ReactElement {
+    const contributorName = isAuthor
+        ? contributor.name
+        : getShortContributorName(contributor.name);
 
     return (
-        <div className={b('one_author')}>
-            {getAvatar(author, vcsType)}
-            <div>{userName}</div>
+        <div className={b('one_contributor')}>
+            {getAvatar(contributor, vcsType)}
+            <div>{contributorName}</div>
         </div>
     );
 }
 
-function getShortUserName(fullUserName: string): string {
-    return fullUserName
+function getShortContributorName(fullContributorName: string): string {
+    return fullContributorName
         .split(' ')
         .reduce((result, current, index) => {
             return index > 0 ? `${result} ${current.charAt(0)}.` : current;
         }, '');
 }
 
-function getHiddenAvatars(authors: Contributor[]): ReactElement | null {
-    const authorCount = authors.length;
+function getHiddenAvatars(contributors: Contributor[]): ReactElement | null {
+    const contributorsCount = contributors.length;
 
-    if (authorCount === 0) {
+    if (contributorsCount === 0) {
         return null;
     }
 
     // TODO: add logic when tooltip will be implemented
 
-    const authorsCountString = authorCount >= LOWER_BOUND_MORE_AUTHORS
-        ? `${authorCount}+`
-        : `+${authorCount}`;
+    const contributorsCountString = contributorsCount >= LOWER_BOUND_MORE_CONTRIBUTORS
+        ? `${contributorsCount}+`
+        : `+${contributorsCount}`;
 
     const hiddenAvatars = (
         <div className={b('avatar', {size: 'small'})}>
-            {authorsCountString}
+            {contributorsCountString}
         </div>
     );
 
     return hiddenAvatars;
 }
 
-function getAvatar(author: Contributor, vcsType?: Vcs): ReactElement {
-    if (!author.avatar) {
-        return getDefaultIcon(author.login, vcsType);
+function getAvatar(contributor: Contributor, vcsType?: Vcs): ReactElement {
+    if (!contributor.avatar) {
+        return getDefaultIcon(contributor.login, vcsType);
     }
 
-    const avatar = <img key={author.login} className={b('avatar', {size: 'small'})} src={author.avatar}/>;
+    const avatar = <img key={contributor.login} className={b('avatar', {size: 'small'})} src={contributor.avatar}/>;
     // TODO: add logic when tooltip will be implemented
 
     return avatar;

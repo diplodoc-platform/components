7c146d559695dbf1beb8981716b7468765607f24
birman111 <birman111@yandex-team.ru>
2022-01-19 07:42:22 +0300

    DOCSTOOLS-190: exclude svg from highlighting

--- src/components/SearchBar/constants.ts
+++ src/components/SearchBar/constants.ts
@@ -5,5 +5,8 @@ export const CLASSNAME_SELECTED = 'dc-search-highlighted_selected';
 export const HIGHLIGHT_OPTIONS = {
     element: 'span',
     className: CLASSNAME,
-    exclude: [`.${CLASSNAME}`], // Exclude the elements to highlight to avoid duplicating the highlight
+    exclude: [
+        `.${CLASSNAME}`, // Exclude the elements to highlight to avoid duplicating the highlight
+        'svg *'
+    ],
 };

3051dc56a59c6d4774696acc05e42bd04de34305
3y3 <3y3@ya.ru>
2023-09-05 19:52:18 +0300

    chore: Remove outdated infrastructure

--- .storybook/webpack-levels/index.js
+++
@@ -1,6 +0,0 @@
-exports.ConfigBuilder = require('./lib/ConfigBuilder');
-exports.javascript = require('./lib/levels/javascript');
-exports.styles = require('./lib/levels/styles');
-exports.assets = require('./lib/levels/assets');
-exports.analyze = require('./lib/levels/analyze');
-exports.optimize = require('./lib/levels/optimize');

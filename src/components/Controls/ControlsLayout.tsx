739c30b2398c71e8d4232926482d7dbbfbcddb11
3y3 <3y3@ya.ru>
2023-12-20 15:39:08 +0300

    feat(Settings): Add settings mobile view

--- src/components/Controls/ControlsLayout.tsx
+++ src/components/Controls/ControlsLayout.tsx
@@ -7,6 +7,7 @@ import {ControlSizes} from '../../models';
 
 type ControlsLayoutProps = {
     isWideView?: boolean;
+    isMobileView?: boolean;
     isVerticalView?: boolean;
     controlClassName?: string;
     controlSize?: ControlSizes;
@@ -16,6 +17,7 @@ type ControlsLayoutProps = {
 export const ControlsLayoutContext = createContext<ControlsLayoutProps>({
     controlClassName: '',
     isWideView: false,
+    isMobileView: false,
     isVerticalView: false,
     controlSize: ControlSizes.M,
     popupPosition: PopperPosition.BOTTOM_END,
@@ -25,6 +27,7 @@ const b = block('dc-controls');
 
 export const ControlsLayout: React.FC<PropsWithChildren<ControlsLayoutProps>> = ({
     isWideView,
+    isMobileView,
     isVerticalView,
     controlClassName,
     controlSize,
@@ -36,6 +39,7 @@ export const ControlsLayout: React.FC<PropsWithChildren<ControlsLayoutProps>> = 
             value={{
                 controlClassName: controlClassName || b('control'),
                 isWideView: isWideView,
+                isMobileView: isMobileView,
                 isVerticalView: isVerticalView,
                 controlSize: controlSize,
                 popupPosition: popupPosition,

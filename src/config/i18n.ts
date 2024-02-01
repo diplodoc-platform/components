1f8e25941711cbe1d5c2eef4da788f0b4b8c1ec4
Martyanov Andrey <37772440+martyanovandrey@users.noreply.github.com>
2024-02-01 19:43:07 +0300

    feat: support he-IL, es-ES, fr-FR, cs-CZ, ar-SA langs (#201)

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -3,7 +3,12 @@ import {initReactI18next} from 'react-i18next';
 
 import {Lang} from '../models';
 
+import ar from '../i18n/ar.json';
+import cs from '../i18n/cs.json';
 import en from '../i18n/en.json';
+import es from '../i18n/es.json';
+import fr from '../i18n/fr.json';
+import he from '../i18n/he.json';
 import ru from '../i18n/ru.json';
 
 export type Loc = Record<string, typeof en>;
@@ -21,6 +26,11 @@ export const configureI18N = ({lang, loc}: I18NConfig) => {
         loc = loc || {
             ru: JSON.parse(ru as unknown as string),
             en: JSON.parse(en as unknown as string),
+            es: JSON.parse(es as unknown as string),
+            fr: JSON.parse(fr as unknown as string),
+            cs: JSON.parse(cs as unknown as string),
+            he: JSON.parse(he as unknown as string),
+            ar: JSON.parse(ar as unknown as string),
         };
 
         initializePromise = i18n.use(initReactI18next).init({

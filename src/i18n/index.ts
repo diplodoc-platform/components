a593eac3b0a0458c83fc6de8264ab075f46d2b81
martyanov-av <martyanov-av@yandex-team.ru>
2024-04-05 16:54:24 +0300

    feat: add tr-TR, kk-KZ, uz-UZ, bg-BG, zh-TW, zh-CN, pt-PT, et-EE, el-GR

---
+++ src/i18n/index.ts
@@ -0,0 +1,35 @@
+import ar from '../i18n/ar.json';
+import bg from '../i18n/bg.json';
+import cs from '../i18n/cs.json';
+import el from '../i18n/el.json';
+import en from '../i18n/en.json';
+import es from '../i18n/es.json';
+import et from '../i18n/et.json';
+import fr from '../i18n/fr.json';
+import he from '../i18n/he.json';
+import kk from '../i18n/kk.json';
+import pt from '../i18n/pt.json';
+import ru from '../i18n/ru.json';
+import tr from '../i18n/tr.json';
+import uz from '../i18n/uz.json';
+import zhTw from '../i18n/zh-tw.json';
+import zh from '../i18n/zh.json';
+
+export default {
+    ar,
+    bg,
+    cs,
+    el,
+    en,
+    es,
+    et,
+    fr,
+    he,
+    kk,
+    pt,
+    ru,
+    tr,
+    uz,
+    zhTw,
+    zh,
+};

fb85e37bb7623ed63cdf3d9778caa40cbcb4172f
Ruslan Bagautdinov <bagautdinovrl@yandex-team.ru>
2024-07-11 16:38:34 +0300

    SubNavigation slides up when scrolling the page

--- src/components/SubNavigation/SubNavigation.tsx
+++ src/components/SubNavigation/SubNavigation.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import {ArrowShapeTurnUpRight, SquareListUl} from '@gravity-ui/icons';
 import {Button} from '@gravity-ui/uikit';
@@ -16,10 +16,52 @@ export interface SubNavigationProps {
     title: string | undefined;
     // shareData: ShareData;
 }
+
 export const SubNavigation = ({
     title,
     // shareData
 }: SubNavigationProps) => {
+    const [visibility, setVisibility] = useState(true);
+    const [hiddingInterval, setHiddingInterval] = useState<NodeJS.Timeout | undefined>(undefined);
+    const [lastScrollY, setLastScrollY] = useState(window.screenY);
+
+    const controlVisibility = useCallback(() => {
+        if (lastScrollY === 0) {
+            setLastScrollY(window.scrollY);
+            setVisibility(true);
+            return;
+        }
+
+        if (window.scrollY > lastScrollY) {
+            if (hiddingInterval) {
+                return;
+            }
+
+            setVisibility(false);
+
+            setHiddingInterval(setTimeout(() => {
+                clearTimeout(hiddingInterval);
+                setHiddingInterval(undefined);
+            }, 1000))
+        } else if (window.scrollY < lastScrollY) {
+            setVisibility(true);
+        } else {
+            return;
+        }
+
+        setLastScrollY(window.scrollY);
+    }, [lastScrollY, hiddingInterval, setLastScrollY, setVisibility, setHiddingInterval]);
+
+
+    useEffect(() => {
+        window.addEventListener('scroll', controlVisibility);
+
+        return () => {
+            window.removeEventListener('scroll', controlVisibility);
+        };
+    }, [controlVisibility]);
+
+
     const shareData = useMemo(() => {
         return {
             title,
@@ -42,22 +84,18 @@ export const SubNavigation = ({
     }, [])
 
     return (
-        <div className={b()}>
-            {/* <div className={b('left')}> */}
-                <button onClick={openMiniTocHandler} type='button' className={b('left')}>
-                    <div className={b('icon')}>
-                        <SquareListUl width={20} height={20} />
-                    </div>
-                    <span className={b('title')}>{title ? title : ""}</span>
-                </button>
-            {/* </div> */}
-            {/* <div className={b('right')}> */}
-                <Button onClick={shareHandler} size="xs" view='flat' className={b('button')}>
-                    <Button.Icon>
-                        <ArrowShapeTurnUpRight width={20} height={20} />
-                    </Button.Icon>
-                </Button>
-            {/* </div> */}
+        <div className={b({ "hidden": !visibility, "visible": visibility })} >
+            <button onClick={openMiniTocHandler} type='button' className={b('left')}>
+                <div className={b('icon')}>
+                    <SquareListUl width={20} height={20} />
+                </div>
+                <span className={b('title')}>{title ? title : ""}</span>
+            </button>
+            <Button onClick={shareHandler} size="xs" view='flat' className={b('button')}>
+                <Button.Icon>
+                    <ArrowShapeTurnUpRight width={20} height={20} />
+                </Button.Icon>
+            </Button>
         </div>
     );
 };

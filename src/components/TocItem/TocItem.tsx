03c7d3dde931d163cbd3efd373c859f7d717af42
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-31 14:42:43 +0300

    feat: add aria-label in TocItem
    Add aria-label on the buttons for showing/hiding
    the table of contents. Add additional "WithAriaLabel"
    component to use "useTranslation" hook in class component.

--- src/components/TocItem/TocItem.tsx
+++ src/components/TocItem/TocItem.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import block from 'bem-cn-lite';
+import {KeyPrefix, Namespace, TFunction} from 'react-i18next';
 
+import {useTranslation} from '../../hooks';
 import {TocItem as ITocItem} from '../../models';
 import {isExternalHref} from '../../utils';
 import {ToggleArrow} from '../ToggleArrow';
@@ -23,47 +25,54 @@ export interface TocItemProps extends ITocItem {
 
 class TocItem extends React.Component<TocItemProps> {
     contentRef = React.createRef<HTMLButtonElement>();
-
     render() {
-        const {name, href, active, expandable, expanded} = this.props;
-        const text = <span>{name}</span>;
-        const icon = expandable ? (
-            <ToggleArrow className={b('icon')} open={expanded} thin={true} />
-        ) : null;
-
-        const content = React.createElement(
-            href ? 'div' : 'button',
-            {
-                ref: href ? null : this.contentRef,
-                className: b('text', {active}),
-                onClick: expandable && !href ? this.handleClick : undefined,
-                'aria-expanded': expandable ? expanded : undefined,
-                tabIndex: expandable ? 0 : -1,
-            },
-            icon,
-            text,
-        );
-
-        if (!href) {
-            return content;
-        }
-
-        const isExternal = isExternalHref(href);
-        const linkAttributes = {
-            href,
-            target: isExternal ? '_blank' : '_self',
-            rel: isExternal ? 'noopener noreferrer' : undefined,
-        };
-
         return (
-            <a
-                {...linkAttributes}
-                className={b('link')}
-                onClick={expandable && href ? this.handleClick : undefined}
-                data-router-shallow
-            >
-                {content}
-            </a>
+            <WithAriaLabel
+                render={(t: TFunction<Namespace<string>, KeyPrefix<Namespace<string>>>) => {
+                    const {name, href, active, expandable, expanded} = this.props;
+                    const text = <span>{name}</span>;
+                    const icon = expandable ? (
+                        <ToggleArrow className={b('icon')} open={expanded} thin={true} />
+                    ) : null;
+
+                    const content = React.createElement(
+                        href ? 'div' : 'button',
+                        {
+                            ref: href ? null : this.contentRef,
+                            className: b('text', {active}),
+                            onClick: expandable && !href ? this.handleClick : undefined,
+                            'aria-expanded': expandable ? expanded : undefined,
+                            'aria-label': expandable ? t('drop-down-list') + name : undefined,
+                            tabIndex: expandable ? 0 : -1,
+                        },
+                        icon,
+                        text,
+                    );
+
+                    if (!href) {
+                        return content;
+                    }
+
+                    const isExternal = isExternalHref(href);
+                    const linkAttributes = {
+                        href,
+                        target: isExternal ? '_blank' : '_self',
+                        rel: isExternal ? 'noopener noreferrer' : undefined,
+                    };
+
+                    return (
+                        <a
+                            {...linkAttributes}
+                            className={b('link')}
+                            onClick={expandable && href ? this.handleClick : undefined}
+                            data-router-shallow
+                            aria-current={active ? 'true' : undefined}
+                        >
+                            {content}
+                        </a>
+                    );
+                }}
+            />
         );
     }
 
@@ -102,6 +111,15 @@ class TocItem extends React.Component<TocItemProps> {
 
         toggleItem(id, expanded);
     };
+}
+
+export interface WithAriaLabelProps {
+    render: (t: TFunction<Namespace<string>, KeyPrefix<Namespace<string>>>) => JSX.Element;
+}
+
+function WithAriaLabel({render}: WithAriaLabelProps) {
+    const {t} = useTranslation('toc-nav-panel');
+    return render(t);
 }
 
 export default TocItem;

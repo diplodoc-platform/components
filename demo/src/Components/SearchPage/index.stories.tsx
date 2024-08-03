186a999659ed017e83de013b16af98031b71d642
Uyama <axelezgames@gmail.com>
2024-08-03 15:52:51 +0300

    feat(demo): add mocks

--- demo/src/Components/SearchPage/index.stories.tsx
+++ demo/src/Components/SearchPage/index.stories.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable no-console */
+
 import React, {useState} from 'react';
 
 import {ISearchItem, SearchPage} from '@diplodoc/components';
 
 import mockData from './data';
+import searchData from './searchData';
 
 const SearchPageDemo = (args) => {
     const isMobile = args['Mobile'];
@@ -29,6 +32,7 @@ const SearchPageDemo = (args) => {
                 relevantOnClick={(item) => console.log('Click on like  button', item)}
                 itemsPerPage={2}
                 totalItems={mockData.length}
+                searchData={searchData}
             />
         </div>
     );

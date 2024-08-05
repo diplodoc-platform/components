f2ce9a2f4e637e27fdfebb3423b20fe569cbbcb8
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-08-05 14:32:52 +0300

    refactor: convert TocItem class component to functional component
    Move "scrollToItem" method from TocItem to Toc
    component because TocItem doesn't call it itself.

--- src/components/TocItem/TocItem.tsx
+++ src/components/TocItem/TocItem.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import block from 'bem-cn-lite';
-import {KeyPrefix, Namespace, TFunction} from 'react-i18next';
 
 import {useTranslation} from '../../hooks';
 import {TocItem as ITocItem} from '../../models';
@@ -23,108 +22,67 @@ export interface TocItemProps extends ITocItem {
     toggleItem: (id: string, opened: boolean) => void;
 }
 
-class TocItem extends React.Component<TocItemProps> {
-    contentRef = React.createRef<HTMLButtonElement>();
+export const TocItem: React.FC<TocItemProps> = React.forwardRef(
+    (
+        {id, name, href, active, expandable, expanded, toggleItem, labeled},
+        ref: React.ForwardedRef<HTMLButtonElement>,
+    ) => {
+        const handleClick = () => {
+            if (!active && href) {
+                return;
+            }
 
-    render() {
-        return (
-            <WithAriaLabel
-                render={(t: TFunction<Namespace<string>, KeyPrefix<Namespace<string>>>) => {
-                    const {name, href, active, expandable, expanded, labeled} = this.props;
-                    const text = <span>{name}</span>;
-                    const icon = expandable ? (
-                        <ToggleArrow className={b('icon')} open={expanded} thin={true} />
-                    ) : null;
+            toggleItem(id, expanded);
+        };
 
-                    const content = React.createElement(
-                        href ? 'div' : 'button',
-                        {
-                            ref: href ? null : this.contentRef,
-                            className: b('text', {
-                                clicable: Boolean(expandable || href),
-                                active,
-                                labeled,
-                            }),
-                            onClick: expandable && !href ? this.handleClick : undefined,
-                            'aria-expanded': expandable ? expanded : undefined,
-                            'aria-label': expandable ? t('drop-down-list') + ' ' + name : undefined,
-                            tabIndex: expandable ? 0 : -1,
-                        },
-                        icon,
-                        text,
-                    );
+        const {t} = useTranslation('toc-nav-panel');
 
-                    if (!href) {
-                        return content;
-                    }
+        const text = <span>{name}</span>;
+        const icon = expandable ? (
+            <ToggleArrow className={b('icon')} open={expanded} thin={true} />
+        ) : null;
 
-                    const isExternal = isExternalHref(href);
-                    const linkAttributes = {
-                        href,
-                        target: isExternal ? '_blank' : '_self',
-                        rel: isExternal ? 'noopener noreferrer' : undefined,
-                    };
-
-                    return (
-                        <a
-                            {...linkAttributes}
-                            className={b('link')}
-                            onClick={expandable && href ? this.handleClick : undefined}
-                            data-router-shallow
-                            aria-current={active ? 'true' : undefined}
-                        >
-                            {content}
-                        </a>
-                    );
-                }}
-            />
+        const content = React.createElement(
+            href ? 'div' : 'button',
+            {
+                ref: href ? null : ref,
+                className: b('text', {
+                    clicable: Boolean(expandable || href),
+                    active,
+                    labeled,
+                }),
+                onClick: expandable && !href ? handleClick : undefined,
+                'aria-expanded': expandable ? expanded : undefined,
+                'aria-label': expandable ? t('drop-down-list') + ' ' + name : undefined,
+                tabIndex: expandable ? 0 : -1,
+            },
+            icon,
+            text,
         );
-    }
 
-    scrollToItem = () => {
-        if (!this.contentRef.current) {
-            return;
+        if (!href) {
+            return content;
         }
 
-        const itemElement = this.contentRef.current;
-        const itemHeight = itemElement.offsetHeight ?? 0;
-        const itemOffset = itemElement.offsetTop;
-        const scrollableParent = itemElement.offsetParent as HTMLDivElement | null;
+        const isExternal = isExternalHref(href);
+        const linkAttributes = {
+            href,
+            target: isExternal ? '_blank' : '_self',
+            rel: isExternal ? 'noopener noreferrer' : undefined,
+        };
 
-        if (!scrollableParent) {
-            return;
-        }
-
-        const scrollableHeight = scrollableParent.offsetHeight;
-        const scrollableOffset = scrollableParent.scrollTop;
-
-        const itemVisible =
-            itemOffset >= scrollableOffset &&
-            itemOffset <= scrollableOffset + scrollableHeight - itemHeight;
-
-        if (!itemVisible) {
-            scrollableParent.scrollTop = itemOffset - Math.floor(scrollableHeight / 2) + itemHeight;
-        }
-    };
-
-    private handleClick = () => {
-        const {id, href, active, expanded, toggleItem} = this.props;
-
-        if (!active && href) {
-            return;
-        }
-
-        toggleItem(id, expanded);
-    };
-}
-
-export interface WithAriaLabelProps {
-    render: (t: TFunction<Namespace<string>, KeyPrefix<Namespace<string>>>) => JSX.Element;
-}
-
-function WithAriaLabel({render}: WithAriaLabelProps) {
-    const {t} = useTranslation('toc-nav-panel');
-    return render(t);
-}
+        return (
+            <a
+                {...linkAttributes}
+                className={b('link')}
+                onClick={expandable && href ? handleClick : undefined}
+                data-router-shallow
+                aria-current={active ? 'true' : undefined}
+            >
+                {content}
+            </a>
+        );
+    },
+);
 
 export default TocItem;

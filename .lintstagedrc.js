cb75a87802044dd7f8a3b247d55df8f84789f3c7
3y3 <3y3@ya.ru>
2024-08-13 00:40:26 +0300

    chore: Update lint package

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@ module.exports = {
     '**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': ['prettier --write', 'eslint --max-warnings=0 --fix'],
     '**/*.{css,scss}': ['prettier --write', 'stylelint --fix'],
     '**/*.{json,yaml,yml,md}': ['prettier --write'],
-    '**/*.{svg}': ['svgo'],
+    '**/*.{svg,svgx}': ['svgo'],
 };
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-    extends: require.resolve('@diplodoc/lint/stylelint-config')
+    extends: require.resolve('@diplodoc/lint/stylelint-config'),
 };

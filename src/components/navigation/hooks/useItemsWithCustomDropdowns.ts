4d119892b4830ea99a4356635ba127726f322af1
Ruslan Bagautdinov <bagautdinovrl@yandex-team.ru>
2024-09-13 02:34:40 +0300

    refactor: added comment that describes need to use any type to MobileDropdown

--- src/components/navigation/hooks/useItemsWithCustomDropdowns.ts
+++ src/components/navigation/hooks/useItemsWithCustomDropdowns.ts
@@ -19,6 +19,9 @@ function addMobileDropdownsTo(items: CustomNavigationItemModel[]) {
             return;
         }
 
+        // 'any' type is needed because here we add an object of a type
+        // that is not in the CustomNavigationItemModel,
+        // but otherwise it is impossible to create your own custom MobileDropdown component
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const newMobileItem: any = {};
         Object.assign(newMobileItem, item);

88c69dc3240b9fa4a017d6bb70c9cb5bf6be3881
Ruslan <88287692+JeikZim@users.noreply.github.com>
2024-09-19 17:47:38 +0300

    fix: generate of url in useShareHandler

--- src/hooks/useShareHandler.ts
+++ src/hooks/useShareHandler.ts
@@ -1,22 +1,14 @@
 import {useCallback, useMemo} from 'react';
 
-import {Router} from '../models';
-
 export type ShareData = {
     title: string;
     url: string;
 };
 
-export const useShareHandler = (title: string, router: Router) => {
+export const useShareHandler = (title: string) => {
     const {hostname, pathname} = router;
 
-    const url: string = useMemo(() => {
-        if (hostname) {
-            return `${hostname}/${pathname}`;
-        }
-
-        return window.location.href;
-    }, [hostname, pathname]);
+    const url: string = typeof window === 'undefined' ? '' : window.location.href;
 
     const shareData: ShareData = useMemo(() => {
         return {

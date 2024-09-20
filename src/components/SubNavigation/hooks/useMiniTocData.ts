2174eb9c7228d7f331ee33eab58fdf5bfdd5be82
Ruslan Bagautdinov <bagautdinovrl@yandex-team.ru>
2024-09-20 13:39:01 +0300

    fix: miniToc close animation, pc-desktop-navigation dropdowns style

--- src/components/SubNavigation/hooks/useMiniTocData.ts
+++ src/components/SubNavigation/hooks/useMiniTocData.ts
@@ -1,40 +1,18 @@
 import {useCallback, useMemo, useState} from 'react';
 
-const useMiniTocData = (
-    pageTitle: string,
-    hideMiniToc: boolean,
-    menuOpen: boolean,
-    // setVisibility: (event: boolean) => void,
-    // onMiniTocItemClick?: (event: MouseEvent) => void,
-) => {
+const useMiniTocData = (pageTitle: string, hideMiniToc: boolean, menuOpen: boolean) => {
     const [miniTocOpen, setMiniTocOpen] = useState(false);
     const [activeMiniTocTitle, setMiniTocTitle] = useState(pageTitle);
 
     const closeMiniToc = useCallback(() => setMiniTocOpen(false), []);
-
-    // const onItemClick = (event: MouseEvent) => {
-    //     if (onMiniTocItemClick) {
-    //         onMiniTocItemClick(event);
-    //     }
-
-    //     setTimeout(() => {
-    //         setVisibility(false);
-    //         closeMiniToc();
-    //     }, 0);
-    // };
 
     const onActiveItemTitleChange = (title: string) => setMiniTocTitle(title);
 
     const toggleMiniTocOpen = useCallback(() => {
         const newState = !miniTocOpen;
 
-        // if (newState) {
-        //     setVisibility(true);
-        // }
-
         setMiniTocOpen(newState);
     }, [miniTocOpen]);
-    // }, [miniTocOpen, setVisibility]);
 
     const miniTocHandler = useMemo(() => {
         if (hideMiniToc || menuOpen) {
@@ -49,7 +27,6 @@ const useMiniTocData = (
         activeMiniTocTitle,
         closeMiniToc,
         miniTocHandler,
-        // onItemClick,
         onActiveItemTitleChange,
     };
 };

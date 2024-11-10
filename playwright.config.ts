5a447d8b4f0676a1bacfb7a89b88876f0158ffd2
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-11-10 17:55:52 +0300

    feat: add dotenv package and update playwright config
    Add webServer property in config. Before starting
    the tests playwright will run and wait for the launch
    of storybook dev server (if storybook is not running).
    
    Server URL is stored in .env file.
    Dotenv package needs for reading URL from .env.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,22 @@
 import {defineConfig, devices} from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({path: path.resolve(__dirname, '.env')});
 
 export default defineConfig({
     testDir: './tests',
     fullyParallel: true,
-    forbidOnly: Boolean(process.env.CI),
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
+    forbidOnly: true,
+    retries: 2,
+    workers: 1,
     reporter: 'html',
+    webServer: {
+        reuseExistingServer: true,
+        command: 'npm run _storybook:watch',
+        url: process.env.BASE_URL,
+        timeout: 120 * 1000,
+    },
     use: {
         trace: 'on-first-retry',
     },

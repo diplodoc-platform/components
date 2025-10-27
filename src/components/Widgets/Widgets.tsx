6742aa68937d0c29d36e317d7c167df1655d5b30
separatrix <nikitalochmatov@gmail.com>
2025-10-27 23:11:13 +0300

    fix: fixed Widgets

--- src/components/Widgets/Widgets.tsx
+++ src/components/Widgets/Widgets.tsx
@@ -1,9 +1,19 @@
+import type {ReactNode} from 'react';
+
 import React from 'react';
 
 import './Widgets.scss';
 
-const Widgets: React.FC = () => {
-    return <div id="dc-widgets" className="dc-widgets" />;
+export interface WidgetsProps {
+    children?: ReactNode;
+}
+
+const Widgets: React.FC<WidgetsProps> = ({children}) => {
+    return (
+        <div id="dc-widgets" className="dc-widgets">
+            {children || <></>}
+        </div>
+    );
 };
 
 export default Widgets;

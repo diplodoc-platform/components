84e23c4f246ca7e73fc8442e06c3e807aa2f48f4
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2025-11-18 10:51:31 +0300

    chore: added tests of some components (#488)
    * chore: added tests of some components
    
    * chore: added screenshots
    
    * chore: fixed test

---
+++ src/components/ContributorAvatars/__tests__/ContributorAvatars.spec.ts
@@ -0,0 +1,44 @@
+import {expect, test} from '@playwright/test';
+
+import {loadDocumentPage} from 'src/components/utils';
+
+test.beforeEach(async ({page}) => {
+    await loadDocumentPage(page);
+});
+
+test('ContributorAvatars test', async ({page}) => {
+    const contributorsBlock = page.locator('.dc-doc-page__page-contributors');
+    const avatar = contributorsBlock
+        .locator('.dc-contributor-avatars__displayed_avatars')
+        .locator('a')
+        .nth(0);
+    const hiddenAvatars = contributorsBlock.locator('.dc-contributor-avatars__hidden_avatars');
+    const box = await contributorsBlock.boundingBox();
+
+    await expect(contributorsBlock).toHaveScreenshot('ContributorAvatars.png', {
+        maxDiffPixelRatio: 0.02,
+    });
+
+    if (!box) {
+        throw new Error('contributorsBlock not found');
+    }
+
+    const clip = {
+        x: Math.max(box.x, 0),
+        y: Math.max(box.y, 0),
+        width: box.width,
+        height: box.height + 130,
+    };
+
+    await avatar.hover();
+    await expect(page).toHaveScreenshot('Avatar-popup.png', {
+        clip,
+        maxDiffPixelRatio: 0.02,
+    });
+
+    await hiddenAvatars.click();
+    await expect(page).toHaveScreenshot('HiddenAvatars-popup.png', {
+        clip,
+        maxDiffPixelRatio: 0.02,
+    });
+});
---
+++ src/components/Feedback/__tests__/FeedbackControls.spec.ts
@@ -0,0 +1,58 @@
+import type {Page} from '@playwright/test';
+
+import {expect, test} from '@playwright/test';
+
+import {loadDocumentPage} from 'src/components/utils';
+
+const viewports = [{name: 'desktop'}, {name: 'mobile', width: 430, height: 932}];
+
+test.beforeEach(async ({page}) => {
+    await loadDocumentPage(page);
+});
+
+viewports.forEach((viewport) => {
+    test.describe(`FeedbackControls test on ${viewport.name} viewport`, () => {
+        test.beforeEach(async ({page}) => {
+            if (viewport.width && viewport.height) {
+                await page.setViewportSize({width: viewport.width, height: viewport.height});
+            }
+        });
+
+        const runPopupTest = async (
+            page: Page,
+            buttonIdx: number,
+            testName: string,
+            isDefault?: boolean,
+        ) => {
+            const controls = page.locator('.dc-feedback__controls');
+            const button = controls.locator('button').nth(buttonIdx);
+
+            await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+            await page.waitForTimeout(200);
+
+            if (isDefault) {
+                await expect(page).toHaveScreenshot(
+                    `FeedbackControls-${viewport.name}-scrolled.png`,
+                    {
+                        maxDiffPixelRatio: 0.01,
+                    },
+                );
+            }
+
+            await button.click();
+            await page.waitForTimeout(200);
+
+            await expect(page).toHaveScreenshot(`${testName}-${viewport.name}-popup.png`, {
+                maxDiffPixelRatio: 0.01,
+            });
+        };
+
+        test('SuccessPopup test', async ({page}) => {
+            await runPopupTest(page, 0, 'SuccessPopup', true);
+        });
+
+        test('DislikeVariantsPopup test', async ({page}) => {
+            await runPopupTest(page, 1, 'DislikeVariantsPopup');
+        });
+    });
+});

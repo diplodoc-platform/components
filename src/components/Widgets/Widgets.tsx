8721079f3a4d2080aeb6712f3eafa7e70222cadb
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2026-04-01 15:06:00 +0300

    fix: made the widget component into a portal (#588)
    * fix: made the widget component into a portal
    
    * fix: removed ref

--- src/components/Widgets/Widgets.tsx
+++ src/components/Widgets/Widgets.tsx
@@ -1,6 +1,7 @@
-import type {ReactNode} from 'react';
+import type {FC, ReactNode} from 'react';
 
-import React from 'react';
+import {useEffect, useState} from 'react';
+import {createPortal} from 'react-dom';
 
 import './Widgets.scss';
 
@@ -8,12 +9,27 @@ export interface WidgetsProps {
     children?: ReactNode;
 }
 
-const Widgets: React.FC<WidgetsProps> = ({children}) => {
-    return (
-        <div id="dc-widgets" className="dc-widgets">
-            {children}
-        </div>
-    );
+const Widgets: FC<WidgetsProps> = ({children}) => {
+    const [container, setContainer] = useState<HTMLElement | null>(null);
+
+    useEffect(() => {
+        let element = document.getElementById('dc-widgets');
+
+        if (!element) {
+            element = document.createElement('div');
+            element.id = 'dc-widgets';
+            element.className = 'dc-widgets';
+            document.body.appendChild(element);
+        }
+
+        setContainer(element);
+    }, []);
+
+    if (!container) {
+        return null;
+    }
+
+    return createPortal(children, container);
 };
 
 export default Widgets;

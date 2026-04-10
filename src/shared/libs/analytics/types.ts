fda4f4146e4f0f89667efacc89b7030a9d0f65ee
Nikita Stenin <stenin.nikita@gmail.com>
2026-04-10 15:36:58 +0300

    feat(analytics): support metrika initialization on client side (#598)

--- src/shared/libs/analytics/types.ts
+++ src/shared/libs/analytics/types.ts
@@ -1,5 +1,6 @@
 export type AnalyticsParams = Record<string, unknown>;
 
 export interface AnalyticsAdapter {
+    init?(): void | PromiseLike<void>;
     send(event: string, params?: AnalyticsParams): void;
 }

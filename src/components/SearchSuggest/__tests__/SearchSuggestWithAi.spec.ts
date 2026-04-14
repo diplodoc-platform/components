dc8cca81fa186a72c57798ecd3dd1b5850ea9c3f
leevayy <maoaoa7@gmail.com>
2026-04-14 12:08:21 +0300

    chore: added screesnshot tests for ai/action params of searchsuggest

---
+++ src/components/SearchSuggest/__tests__/SearchSuggestWithAi.spec.ts
@@ -0,0 +1,31 @@
+import {expect, test} from '@playwright/test';
+
+import {SEARCH_SUGGEST_WITH_AI_URL} from 'src/components/constants';
+import {loadDocumentPage} from 'src/components/utils';
+
+test.beforeEach(async ({page}) => {
+    await loadDocumentPage(page, SEARCH_SUGGEST_WITH_AI_URL);
+});
+
+test('SearchSuggestWithAi test', async ({page}) => {
+    const searchSuggest = page.locator('.dc-search-suggest__wrapper');
+    const input = searchSuggest.locator('input.g-text-input__control_type_input');
+
+    await expect(page).toHaveScreenshot('SearchSuggestWithAi-default.png', {
+        maxDiffPixelRatio: 0.01,
+    });
+
+    await input.fill('test');
+
+    await expect(page.getByText('Спросить у AI').first()).toBeVisible({timeout: 5000});
+
+    await expect(page).toHaveScreenshot('SearchSuggestWithAi-search.png', {
+        maxDiffPixelRatio: 0.01,
+    });
+
+    await page.locator('body').click();
+
+    await expect(page).toHaveScreenshot('SearchSuggestWithAi-out.png', {
+        maxDiffPixelRatio: 0.01,
+    });
+});

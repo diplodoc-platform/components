30ec4c86a0b69252a996a3a81fcdc7dff5f2c519
Yurok868 <yurok8@yandex-team.ru>
2026-07-10 14:52:58 +0300

    fix: add target (#685)

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -6,6 +6,8 @@ export const DOC_PAGE_GALLERY_URL =
     BASE_URL + '/iframe.html?args=&id=pages-docpagegallery--doc-page-gallery&viewMode=story';
 export const DOC_PAGE_HIDDEN_URL =
     BASE_URL + '/iframe.html?args=&id=pages-docpagehidden--doc-page-hidden&viewMode=story';
+export const DOC_LEADING_PAGE_URL =
+    BASE_URL + '/iframe.html?args=&id=pages-leading--leading&viewMode=story';
 export const DOC_LEADING_PAGE_HIDDEN_URL =
     BASE_URL + '/iframe.html?args=&id=pages-leadinghidden--leading-hidden&viewMode=story';
 export const DOC_PAGE_HEADER_HIDDEN_URL =

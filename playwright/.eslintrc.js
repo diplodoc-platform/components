9a8d4e7bddf7da2fda75e2431a1f6cb251ab2eac
stenin-nikita <stenin.nikita@gmail.com>
2026-07-13 15:10:49 +0300

    chore: fix eslint config for multiple tsconfig files

--- playwright/.eslintrc.js
+++ playwright/.eslintrc.js
@@ -1,12 +1,8 @@
 const path = require('node:path');
 
 module.exports = {
-    extends: require.resolve('@diplodoc/infra/eslint-config/client'),
     parserOptions: {
         tsconfigRootDir: path.resolve(__dirname, '..'),
         project: false,
-    },
-    rules: {
-        'react/react-in-jsx-scope': 'off',
     },
 };
